f6524248145cf0a18f5e3077c77e3f7031fd2ae7
Alex <sanches0796@gmail.com>
2024-08-19 17:38:55 +0300

    cosmetic#2

--- ip.h
+++ ip.h
@@ -2,26 +2,26 @@
 #define IP_H
 
 #include "generic-field.h"
-#include "string.h"
 #include "string-array.h"
+#include "string.h"
 
-class IP: public GenericField {
+class IP : public GenericField {
 
-private:
+  private:
     String ip_rule;
     String ip_name;
     String ip_value;
-public:
+
+  public:
     IP();
 
-    IP(const char*);
-    IP(String&);
+    IP(const char *);
+    IP(String &);
     ~IP();
 
-    bool match(const GenericString& packet) const;    
+    bool match(const GenericString &packet) const;
 
-    bool is_mask(const GenericString&,const GenericString&) const  ;
-
+    bool is_mask(const GenericString &, const GenericString &) const;
 };
 
 #endif
